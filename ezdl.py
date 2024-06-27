7c3e6a87b9ffe395c128209fb4fa77f3c66664d0
Lukas aka sticky aka MoeJoe <96546774+nrdrch@users.noreply.github.com>
2024-06-27 11:10:50 +0200

    Update and rename main.py to ezdl.py

--- ezdl.py
+++ ezdl.py
@@ -6,17 +6,17 @@ from rich.spinner import Spinner
 
 # Initialize rich console
 console = Console()
-
+script_dir = os.path.dirname(__file__)
 # Define the path for settings.toml
-settings_path = 'settings.toml'
+settings_path = os.path.join(os.path.dirname(__file__), 'settings.toml')
 
 # Check if settings.toml exists, if not create it with default values
 def create_default_settings():
     console.print("Creating [bold yellow]settings.toml[/bold yellow] as it does not exist.")
     settings = {
         "paths": {
-            "music_vault_path": r"$HOME\Music\ez-dl-Music",
-            "video_vault_path": r"$HOME\Videos\ez-dl-Videos"
+            "audio_vault_path": r"$HOME\ezdl\Audio",
+            "video_vault_path": r"$HOME\ezdl\Video"
         },
         "settings": {
             "audio_quality": "0",
@@ -45,9 +45,11 @@ with open(settings_path, 'r') as f:
 SPINNER = settings['other']['loading_animation']
 AUDIO_ALIAS = settings['aliases']['audio_download_alias']
 VIDEO_ALIAS = settings['aliases']['video_download_alias']
+AUDIO_VAULT_PATH = settings['paths']['audio_vault_path']
+VIDEO_VAULT_PATH = settings['paths']['video_vault_path']
 # Execute yt-dlp command based on user input
 def execute_command(command, link, with_playlist):
-    command = command.replace("{music_vault_path}", settings['paths']['music_vault_path'])
+    command = command.replace("{audio_vault_path}", settings['paths']['audio_vault_path'])
     command = command.replace("{video_vault_path}", settings['paths']['video_vault_path'])
     command = command.replace("{audio_quality}", settings['settings']['audio_quality'])
     command = command.replace("{audio_format}", settings['settings']['audio_format'])
@@ -56,35 +58,42 @@ def execute_command(command, link, with_playlist):
     if with_playlist:
         command = command.replace("--no-playlist", "")
     
-    with console.status("[bold cyan]Running Download command...[/bold cyan]", spinner=f"{SPINNER}", spinner_style="status.spinner", speed=1.0, refresh_per_second=12.5):
+    with console.status("[bold cyan]Downloading[/bold cyan]", spinner=f"{SPINNER}", spinner_style="status.spinner", speed=1.0, refresh_per_second=12.5):
         try:
             result = subprocess.run(['powershell', '-Command', command], capture_output=True, text=True)
             if result.returncode == 0:
-                console.print("[bold green]:white_check_mark:[/bold green] Download complete!")
+                console.print(":white_check_mark:[bold green] log [/bold green]Download complete!")
             else:
-                console.print(f"[bold red]:x: Failed![/bold red] {result.stderr}")
+                console.print(f":x:[bold red] log [/bold red]{result.stderr}")
         except Exception as e:
-            console.print(f"[bold red]:x: Exception![/bold red] {str(e)}")
+            console.print(f":x:[bold red] log [/bold red]{str(e)}")
 
 # Main function to parse arguments and call the appropriate command
 def main():
     import sys
     if len(sys.argv) < 2:
-        console.print("[bold red]:x:[/bold red] No arguments provided. Use '--help' for usage information.")
+        console.print(":x:[bold red] log [/bold red]No arguments provided. Use '--help' for usage information.")
         return
     
     if sys.argv[1] in ['--help', '-h']:
         console.print("[bold white]yt-dlp wrapper for simplicity[/bold white] \n"
                       "[bold cyan]Usage:[/bold cyan]\n"
-                      " [#076841]ez-dl [/#076841]" f"{AUDIO_ALIAS}" " <[#DBC75D]link[/#DBC75D]> \n"
-                      " [#076841]ez-dl [/#076841]" f"{VIDEO_ALIAS}" " <[#DBC75D]link[/#DBC75D]> \n"
-                      "[#B1B1B1]optionally run with 'wp' or 'withplaylist', to download the whole playlist:[/#B1B1B1]\n"
-                      " [#076841]ez-dl [/#076841]" f"{AUDIO_ALIAS}" " <[#DBC75D]link[/#DBC75D]> [#DBC75D]wp[/#DBC75D] \n"
-                      " [#076841]ez-dl [/#076841]" f"{VIDEO_ALIAS}" " <[#DBC75D]link[/#DBC75D]> [#DBC75D]wp[/#DBC75D] \n")
+                      " [#076841]ezdl [/#076841]" f"{AUDIO_ALIAS}" " <[#DBC75D]link[/#DBC75D]> \n"
+                      " [#076841]ezdl [/#076841]" f"{VIDEO_ALIAS}" " <[#DBC75D]link[/#DBC75D]> \n"
+                      "\n"
+                      "[bold white]optionally run with[/bold white] [#DBC75D]'wp'[/#DBC75D] [bold white]or[/bold white] [#DBC75D]'withplaylist'[/#DBC75D][bold white], to download the whole playlist:[/bold white]\n"
+                      " [#076841]ezdl [/#076841]" f"{AUDIO_ALIAS}" " <[#DBC75D]link[/#DBC75D]> [#DBC75D]wp[/#DBC75D] \n"
+                      " [#076841]ezdl [/#076841]" f"{VIDEO_ALIAS}" " <[#DBC75D]link[/#DBC75D]> [#DBC75D]wp[/#DBC75D] \n"
+                      "\n"
+                      "[bold cyan]Locations:[/bold cyan]\n"
+                      ":wrench: [#A0A0A0]Settings [/#A0A0A0]" f"'{settings_path}'\n"
+                      ":musical_note: [#A0A0A0]Audio    [/#A0A0A0]" f"'{AUDIO_VAULT_PATH}'" "\n"             
+                      ":movie_camera: [#A0A0A0]Video    [/#A0A0A0]" f"'{VIDEO_VAULT_PATH}'" "\n"             
+                      )
         return
 
     if len(sys.argv) < 3:
-        console.print("[bold red]:x:[/bold red] Invalid number of arguments. Use '--help' for usage information.")
+        console.print(":x:[bold red] log [/bold red]Invalid number of arguments. Use '--help' for usage information.")
         return
     
     alias = sys.argv[1]
@@ -92,13 +101,11 @@ def main():
     with_playlist = len(sys.argv) > 3 and (sys.argv[3] in ['wp', 'withplaylist'])
 
     if alias == settings['aliases']['audio_download_alias']:
-        command = '$null = yt-dlp -x --audio-quality {audio_quality} --audio-format {audio_format} --ignore-errors --output "{music_vault_path}\\%(title)s.%(ext)s" --no-playlist "<youtube_link>"'
+        command = '$null = yt-dlp -x --audio-quality {audio_quality} --audio-format {audio_format} --ignore-errors --output "{audio_vault_path}\\%(title)s.%(ext)s" --no-playlist "<youtube_link>"'
     elif alias == settings['aliases']['video_download_alias']:
         command = '$null = yt-dlp --ignore-errors --remux-video {video_format} --output "{video_vault_path}\\%(title)s.%(ext)s" --no-playlist "<youtube_link>"'
-    elif alias == "open":
-        command = ''
     else:
-        console.print("[bold red]:x:[/bold red] Unknown alias.")
+        console.print(":x:[bold red] log [/bold red]Unknown alias.")
         return
     
     execute_command(command, link, with_playlist)
