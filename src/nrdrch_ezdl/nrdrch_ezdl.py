117b9f1edd0ca21680d3f95fe09bf3d448d9c8ec
jds4nrdrch <96546774+nrdrch@users.noreply.github.com>
2024-06-30 16:29:07 +0200

    Update
    Did Stuff but now just uploading --help pages improvement

--- src/nrdrch_ezdl/nrdrch_ezdl.py
+++ src/nrdrch_ezdl/nrdrch_ezdl.py
@@ -5,12 +5,22 @@ from rich import box
 from rich.table import Table
 from rich.spinner import Spinner
 from shutil import copyfile
+from rich.tree import Tree
 from datetime import datetime
-VERSION_ID = "v1.1.8"
+from rich.tree import Tree
+from rich.panel import Panel
+from rich import inspect
+#from prompt_toolkit import prompt
+#from prompt_toolkit.key_binding import KeyBindings
+#from prompt_toolkit.keys import Keys
+
+VERSION_ID = "v1.1.9"
+
+
+
 
 console = Console()
 script_dir = os.path.dirname(__file__)
-# Define the path for settings.toml
 settings_path = os.path.join(os.path.dirname(__file__), 'settings.toml')
 version_file = os.path.join(os.path.dirname(__file__), f'{VERSION_ID}')
 timestamp = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
@@ -25,9 +35,10 @@ default_settings = {
         "audio_quality": "0",
         "audio_format": "wav",
         "video_format": "mp4",
-        "file_naming_scheme": "%(title)s - %(duration)s",
+        "file_naming_scheme": "%(title)s",
         "open_folder_after_download": "no",
-        "use_original_thumbnail": "no"
+        "use_original_thumbnail": "no",
+        "terminal_size_error": "yes"
     },
     "aliases": {
         "audio_download_alias": "audio",
@@ -38,7 +49,12 @@ default_settings = {
     },
     "other": {
         "loading_animation": "aesthetic"
-    }
+    },
+    "color": {
+        "settings_cln_clr": "#E7E7E7",
+        "description_clr_1": "#417543",
+        "description_clr_2": "#eb144c"
+    }  
 }
 def create_default_settings():
     with open(settings_path, 'w', encoding='utf-8') as f:
@@ -67,28 +83,22 @@ if not os.path.exists(version_file):
         pass
     console.print(f":party_popper: Version [bold cyan]{VERSION_ID}[/bold cyan] installed! ")
 
-    # Load settings from files
-    # Load settings from files
     with open(settings_backup, 'r', encoding='utf-8') as f:
         current_settings = toml.load(f)
-    # Compare and merge settings
     updated_settings = merge_settings(default_settings, current_settings)
-    # Save updated settings
-    # Save updated settings
     with open(settings_path, 'w', encoding='utf-8') as f:
         toml.dump(updated_settings, f)
     if not filecmp.cmp(os.path.normpath(settings_path), os.path.normpath(settings_backup), shallow=False):
         console.print(":warning: [bold red] Warning:[/bold red] The default settings changed!")
         console.print(f"[bold white]BACKUP PATH: [/bold white] [bold yellow]{os.path.normpath(settings_backup)}[/bold yellow]")
         console.print("")
-
-
 # Load settings from settings.toml
 with open(settings_path, 'r', encoding='utf-8') as f:
     settings = toml.load(f)
 AUDIO_FORMAT = settings['settings']['audio_format']
 VIDEO_FORMAT = settings['settings']['video_format']
 SPINNER = settings['other']['loading_animation']
+T_SIZE_ERROR = settings['settings']['terminal_size_error']
 AUDIO_ALIAS = settings['aliases']['audio_download_alias']
 VIDEO_ALIAS = settings['aliases']['video_download_alias']
 AUDIO_OPEN_ALIAS = settings['aliases']['audio_open_alias']
@@ -96,7 +106,9 @@ VIDEO_OPEN_ALIAS = settings['aliases']['video_open_alias']
 SETTINGS_OPEN_ALIAS = settings['aliases']['settings_open_alias']
 EMBED_THUMBNAILS = settings['settings']['use_original_thumbnail']
 OPEN_AFTER_DL = settings['settings']['open_folder_after_download']
-
+SETTINGS_COLOR = settings['color']['settings_cln_clr']
+DESC_CLR_1 = settings['color']['description_clr_1']
+DESC_CLR_2 = settings['color']['description_clr_2']
 # Function to expand PowerShell variables
 def expand_powershell_variables(value):
     if isinstance(value, str) and value.startswith('$'):
@@ -115,6 +127,18 @@ def expand_powershell_variables(value):
                 return value  # Return original value if variable not found
     return value  # Return unchanged if not starting with '$'
 # Function to handle path expansion based on type
+
+# Function to expand paths
+# Function to expand paths
+def expand_path(value):
+    if isinstance(value, str):
+        return expand_powershell_variables(value)
+    else:
+        return value
+
+# Function to add directory contents to a tree
+
+# Function to expand paths
 def expand_path(value):
     if isinstance(value, str):
         return expand_powershell_variables(value)
@@ -122,6 +146,7 @@ def expand_path(value):
         return value
 AU_PATH = expand_path(os.path.expanduser(settings['paths']['audio_vault_path']))
 VID_PATH = expand_path(os.path.expanduser(settings['paths']['video_vault_path']))
+
 def execute_command(command, link, with_playlist):
     command = command.replace("{audio_vault_path}", AU_PATH)
     command = command.replace("{video_vault_path}", VID_PATH)
@@ -194,25 +219,58 @@ def main():
         return
     if sys.argv[1] in ['--help-settings', '-hs']:
         try:
+            
+            
+            current_lns = os.get_terminal_size().lines
+            current_cls = os.get_terminal_size().columns
+            
+            recommended_lns = 24
+            recommended_cls = 158
+            #if current_cls < recommended_cls or current_lns < recommended_lns:
+            #    console.print("[bold yellow]height[/bold yellow]/[bold blue]width[/bold blue]", f'[bold blue]{current_lns}[/bold blue]', '/', f'[bold yellow]{current_cls}[/bold yellow]')
+            #    console.print(f"[bold red]Recommended terminal size is at least {recommended_lns} lines and {recommended_cls} columns[/bold red]")
+            def modify_dim_style(hex_color, brightness_percent):
+                hex_color = hex_color.lstrip('#')
+                rgb = tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
+                factor = (100 + brightness_percent) / 100
+                brightened_rgb = tuple(min(255, int(c * factor)) for c in rgb)
+                return '#{:02x}{:02x}{:02x}'.format(*brightened_rgb)
+
+            row_styles = ["dim", ""]
+
+            # Example usage:
+            dim_color = modify_dim_style("#555555", 20)  # 20% brighter
             with open(settings_path, 'r', encoding='utf-8') as f:
-                settings = toml.load(f)
-
-            table = Table(show_header=True, header_style="bold cyan", box=box.SIMPLE_HEAD)
-            table.add_column("Setting", style="magenta", justify="left", width=45)
-            table.add_column("Description", style="#189255", justify="left", width=57)
-            table.add_column("Options & Usage Examples", style="bold white", justify="left", width=96)
-
+                settings = toml.load(f)            
+            table = Table(show_header=True , header_style="bold cyan", show_lines=False,  show_footer=False,  box=box.SIMPLE_HEAVY)
+            table.add_column("Setting", style=f"{SETTINGS_COLOR}", justify="left", min_width=46, width=34)
+            table.add_column("Description", style=f"{DESC_CLR_1}", justify="left", min_width=55, width=55)
+            table.add_column("Options & Examples", style="bold white", justify="left", min_width=60, width=60)
+            
             for section, values in settings.items():
                 for key, value in values.items():
                     description, options = get_setting_description(section, key)
                     table.add_row(f"{key} = {value}", description, options)
+            
 
+            
             console.print(table)
+            if T_SIZE_ERROR == 'yes':
+                if current_cls < recommended_cls or current_lns < recommended_lns:
+                    console.print("[bold yellow]lines[/bold yellow]/[bold blue]colmns[/bold blue]:", f'[bold yellow]{current_lns}[/bold yellow]', '/', f'[bold blue]{current_cls}[/bold blue]')
+                    console.print(f":x:[bold red] log [/bold red]Recommended terminal size is at least {recommended_lns} lines and {recommended_cls} columns  ")
+            
 
         except Exception as e:
             console.print(f":x:[bold red] Exception! [/bold red] {str(e)}")
         return
 
+        
+    if sys.argv[1] in ['--print-size', '-ps']:
+        current_lns = os.get_terminal_size().lines
+        current_cls = os.get_terminal_size().columns
+        console.print(f'Lines [bold cyan]{current_lns}[/bold cyan] | Columns [bold cyan]{current_cls}[/bold cyan]')
+        return
     
     if len(sys.argv) < 3:
 
@@ -262,37 +320,45 @@ def main():
                 subprocess.run(['explorer', openpath])
                 console.print(f":heavy_check_mark: [bold green] log [/bold green]Opened path at [bold yellow]{openpath}[/bold yellow]")
     except Exception as e:
-        console.print(f":x:[bold red] Exception! [/bold red] {str(e)}")
+        console.print(f':x: [bold red] Exception! [/bold red] {str(e)}')
+
 
 def get_setting_description(section, key):
     descriptions = {
         'paths': {
-            'audio_vault_path': ('Audio [red]save[/ red] location.', r'Set [yellow]Path[/yellow] with doubled right-slashes ("[bold green] \\ [/bold green]") [red]OR[/red] single left-slashes ("[bold green] / [/bold green]")  [red]OR[/red] set a [yellow]Variable[/yellow] by full name.'),
-            'video_vault_path': ("Video [red]save[/ red] location.", r'[#A0A0A0]To define [/#A0A0A0][yellow]variables[/yellow], [#A0A0A0]run this to open PSProfile[/#A0A0A0]: [yellow]notepad[/yellow] [green]$PROFILE[/green] ; define in normal path-format: [green]$env:yourvarname[/green] = "WindowsPathToDir" '),
+            'audio_vault_path': (f'Audio [{DESC_CLR_2}]save[/{DESC_CLR_2}] location.', f'Set a [yellow]Path[/yellow] with double slashes:[bold green] {r'\\'} [/bold green] [bold yellow]{r'C:\\Your\\Path'}[/bold yellow]               [{DESC_CLR_2}]or[/{DESC_CLR_2}] Set a  Environment Variable[yellow]$env:yourvar[/yellow]'),
+            'video_vault_path': (f'Video [{DESC_CLR_2}]save[/{DESC_CLR_2}] location.', ''),
         },
         'settings': {
-            'audio_quality': ('Audio [red]quality[/red] level', '[green]0[/green] - [red]10[/red] ([bold yellow]10[/bold yellow] = [bold red]worst[/bold red]) '),
-            'audio_format': ('Select preffered [red]file format[/red] for Audio.', "[green]wav[/green][bold white],[/bold white] [green]flac[/green][bold white],[/bold white] [yellow]mp3[/yellow][bold white],[/bold white] [yellow]opus[/yellow][bold white],[/bold white] [yellow]ogg[/yellow][bold white],[/bold white][yellow] mka[/yellow][bold white],[/bold white] [yellow]m4a[/yellow][bold white] or [/bold white][yellow]mov[/yellow]"),
-            'video_format': ('Select preffered [red]file format[/red] for Video.', "[green]mp4[/green][bold white],[/bold white] [green]m4v[/green][bold white] or[/bold white] [yellow]mov[/yellow]"),
-            'file_naming_scheme': ('The [red]naming-pattern[/red] after which files are created; use format: [green]%([bold yellow]option1[/bold yellow])s[/green][red]-[/red][green]%([bold yellow]option2[/bold yellow])s[/green][red]-[/red][green]%([bold yellow]option3[/bold yellow])s[/green].', "[yellow]title[/yellow], [yellow]id[/yellow], [yellow]ext[/yellow], [yellow]uploader[/yellow], [yellow]upload_date[/yellow], [yellow]channel[/yellow], [yellow]channel_id[/yellow], [yellow]view_count[/yellow], [yellow]like_count[/yellow], [yellow]duration[/yellow], [yellow]playlist[/yellow], [yellow]playlist_index[/yellow], [yellow]playlist_id[/yellow]"),
-            'open_folder_after_download': ('Open the [red]target directory[/red] after download is [red]done[/red].', "[green]yes[/green] or [red]no[/red]"),
-            'use_original_thumbnail': ('Use [red]original thumbnail[/red].', "[green]yes[/green] or [red]no[/red]"),
+            'audio_quality': (f'Audio [{DESC_CLR_2}]quality[/{DESC_CLR_2}] level', f'[green]0[/green] - [{DESC_CLR_2}]10[/{DESC_CLR_2}] ([bold yellow]10[/bold yellow] = [{DESC_CLR_2}]worst[/{DESC_CLR_2}]) '),
+            'audio_format': (f'Select preffered [{DESC_CLR_2}]file format[/{DESC_CLR_2}] for Audio.', "[green]wav[/green][bold white],[/bold white] [green]flac[/green][bold white],[/bold white] [yellow]mp3[/yellow][bold white],[/bold white] [yellow]opus[/yellow][bold white],[/bold white] [yellow]ogg[/yellow][bold white],[/bold white][yellow] mka[/yellow][bold white],[/bold white] [yellow]m4a[/yellow][bold white] or [/bold white][yellow]mov[/yellow]"),
+            'video_format': (f'Select preffered [{DESC_CLR_2}]file format[/{DESC_CLR_2}] for Video.', "[green]mp4[/green][bold white],[/bold white] [green]m4v[/green][bold white] or[/bold white] [yellow]mov[/yellow]"),
+            'file_naming_scheme': (f'The [{DESC_CLR_2}]naming-pattern[/{DESC_CLR_2}] after which files are created; use format: [green]%([bold yellow]title[/bold yellow])s[/green][{DESC_CLR_2}]-[/{DESC_CLR_2}][green]%([bold yellow]id[/bold yellow])s[/green][{DESC_CLR_2}]-[/{DESC_CLR_2}][green]%([bold yellow]ext[/bold yellow])s[/green].', "[yellow]uploader[/yellow], [yellow]upload_date[/yellow], [yellow]channel[/yellow], [yellow]channel_id[/yellow], [yellow]view_count[/yellow], [yellow]like_count[/yellow], [yellow]duration[/yellow], [yellow]playlist[/yellow], [yellow]playlist_index[/yellow], [yellow]playlist_id[/yellow]"),
+            'open_folder_after_download': (f'Open the [{DESC_CLR_2}]target directory[/{DESC_CLR_2}] after download is [{DESC_CLR_2}]done[/{DESC_CLR_2}].', f"[green]yes[/green] or [{DESC_CLR_2}]no[/{DESC_CLR_2}]"),
+            'use_original_thumbnail': (f'Use [{DESC_CLR_2}]original thumbnail[/{DESC_CLR_2}].', f"[green]yes[/green] or [{DESC_CLR_2}]no[/{DESC_CLR_2}]"),
+            'terminal_size_error': (f'If [{DESC_CLR_2}]yes[/{DESC_CLR_2}], warns if [{DESC_CLR_2}]Window is too small[/{DESC_CLR_2}] for the Table-', '')
         },
         'aliases': {
-            'audio_download_alias': ('Alias for [red]donwloading audio[/red] only.', '[green]Any Word[/green] should work, avoid spaces or special characters'),
-            'video_download_alias': ('Alias for [red]downloading video[/red].', '[green]Any Word[/green] should work, avoid spaces or special characters'),
-            'audio_open_alias': ("Alias for [red]navigating[/red] to the [red]Audio-path[/red].", "[green]Any Word[/green] should work, avoid spaces or special characters"),
-            'video_open_alias': ('Alias for [red]navigating[/red] to the [red]Video-path[/red].','[green]Any Word[/green] should work, avoid spaces or special characters'),
-            'settings_open_alias': ('Alias for [red]navigating[/red] to the [red]Settings-path[/red].', '[green]Any Word[/green] should work, avoid spaces or special characters'),
+            'audio_download_alias': (f'Alias for [{DESC_CLR_2}]donwloading audio[/{DESC_CLR_2}] only.', '[#399EC5]Any Word[/#399EC5] [#A0A0A0] avoid spaces or special characters[/#A0A0A0]'),
+            'video_download_alias': (f'Alias for [{DESC_CLR_2}]downloading video[/{DESC_CLR_2}].', '[#399EC5]Any Word[/#399EC5] [#636363] avoid spaces or special characters[/#636363]'),
+            'audio_open_alias': (f"Alias for [{DESC_CLR_2}]navigating[/{DESC_CLR_2}] to the [{DESC_CLR_2}]Audio-path[/{DESC_CLR_2}].", "[#399EC5]Any Word[/#399EC5] [#353535] avoid spaces or special characters[/#353535]"),
+            'video_open_alias': (f'Alias for [{DESC_CLR_2}]navigating[/{DESC_CLR_2}] to the [{DESC_CLR_2}]Video-path[/{DESC_CLR_2}].','[#399EC5]Any Word[/#399EC5] [#191919] avoid spaces or special characters[/#191919]'),
+            'settings_open_alias': (f'Alias for [{DESC_CLR_2}]navigating[/{DESC_CLR_2}] to the [{DESC_CLR_2}]Settings-path[/{DESC_CLR_2}].', '[#399EC5]Might be:[/#399EC5] [green]au[/green], [green]vi[/green], [green]joemama[/green], [green]whatever[/green]'),
         },
         'other': {
-            'loading_animation': ('[red]Loading animation[/red] style ', '[#A0A0A0]To preview different animations run [/#A0A0A0]: [yellow]python[/yellow] -m [green]rich.spinner[/green] '),
+            'loading_animation': (f'[{DESC_CLR_2}]Loading animation[/{DESC_CLR_2}] style ', '[#A0A0A0]Preview animations with[/#A0A0A0]: [yellow]python[/yellow] -m [green]rich.spinner[/green] '),
+        },
+        'color': {
+            'settings_cln_clr': (f'[{DESC_CLR_2}]Setting[/{DESC_CLR_2}] column color ', '[#399EC5]Hex Values[/#399EC5]: https://colors-picker.com/hex-color-picker/'),
+            'description_clr_1': (f'[{DESC_CLR_2}]Description[/{DESC_CLR_2}] color 1 [yellow]default[/yellow] ', '[#399EC5]Standard Colors by name & more[/#399EC5]: '),
+            'description_clr_2': (f'[{DESC_CLR_2}]Description[/{DESC_CLR_2}] color 2 [yellow]highlights[/yellow] ', 'https://rich.readthedocs.io/en/stable/appendix/colors.html'),
         }
-    }
 
+    }
     if section in descriptions and key in descriptions[section]:
         return descriptions[section][key]
     else:
         return '', ''  
 if __name__ == "__main__":
     main()
+
